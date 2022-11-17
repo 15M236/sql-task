172bc7e2933f955e777443ba2a404ef9e2f57d2e
raghav_8197 <raghavanparamasivam@gmail.com>
2022-11-18 00:57:02 +0530

    Add files via upload

---
+++ sql-task.sql
@@ -0,0 +1,11 @@
+-- create table User1 (
+-- 	id int auto_increment ,
+--     name varchar(255),
+--     email varchar(255) not null default 'rag@gmail.com',
+--     mobile varchar(255),
+--     age int ,
+--     primary key (id),
+-- 	unique(mobile)
+-- )
+
+Insert into user1 (name,mobile,age) values ('nag','9994310924',24) 
